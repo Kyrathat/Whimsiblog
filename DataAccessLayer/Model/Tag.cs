8cc98f0cda346ce0fbfa382f0f7e29d88ed25257
Kyrathat <kyrajaneyoung001@gmail.com>
2025-10-11 12:21:43 -0500

    See standup.

--- DataAccessLayer/Model/Tag.cs
+++ DataAccessLayer/Model/Tag.cs
@@ -14,5 +14,7 @@ namespace DataAccessLayer.Model
         [Required(ErrorMessage = "Name is required.")]
         [StringLength(50, ErrorMessage = "Name can't be longer than 50 characters.")]
         public string? Name { get; set; } = string.Empty;
+
+        public ICollection<BlogPost> BlogPosts { get; set; } = new List<BlogPost>();
     } 
 }
