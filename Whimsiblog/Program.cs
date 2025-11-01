563ff7e1d74bcfa6800e33bcc39d14a0ae0dad8f
Kyrathat <kyrajaneyoung001@gmail.com>
2025-11-01 10:08:30 -0500

    Disabled 18+ filter as it's working completely as intended but I fear we may need to rework it to be more understanding

--- Whimsiblog/Program.cs
+++ Whimsiblog/Program.cs
@@ -1,6 +1,5 @@
-using DataAccessLayer.DataAccess;
+﻿using DataAccessLayer.DataAccess;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
@@ -9,58 +8,45 @@ using Microsoft.Identity.Web.UI;
 using Whimsiblog.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<BlogContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
+builder.Logging.AddConsole();
+
+// Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BlogContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Authentication & Azure AD
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
-// Allow the Hello___! Banner to display a userName instead
 builder.Services.Configure<OpenIdConnectOptions>(
     OpenIdConnectDefaults.AuthenticationScheme,
     options => options.TokenValidationParameters.NameClaimType = "name");
 
-
+// Razor + Identity UI
 builder.Services.AddRazorPages()
     .AddMicrosoftIdentityUI();
 
-// - - - - - - - - - - 18+ Section - - - - - - - - - - 
-
-// Add the 18+ validation onto the whole site
+// Authorization (Age 18+)
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("Age18+",
-        policy => policy.Requirements.Add(new AgeRequirement(18))); // Set the age of the validation (18+)
+        policy => policy.Requirements.Add(new AgeRequirement(18)));
 });
-//  /!\ Handler lifetime MUST be scoped if it uses BlogContext /!\
 builder.Services.AddScoped<IAuthorizationHandler, AgeRequirementHandler>();
 
-// Add services to the container.
+// Controllers with global authorization policy
 builder.Services.AddControllersWithViews(o =>
 {
-    o.Filters.Add(new AuthorizeFilter("Age18+"));
+    o.Filters.Add(new AuthorizeFilter());
 });
-
-// - - - - - - - - - - End Section - - - - - - - - - - 
-
-// Try to read a dedicated connection string for the DefaultConnection.
-var blogConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Register BlogContext in the DI container, telling EF Core to use SQL Server
-// with the resolved connection string above. This lets you inject BlogContext
-// (e.g., into controllers) and have it connect to the right database.
-builder.Services.AddDbContext<BlogContext>(options =>
-    options.UseSqlServer(blogConnection));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -74,8 +60,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
-);
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
